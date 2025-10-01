e6c94ee9257224d6c5c637d27b1344c199066921
Jihoon <kontang5@icloud.com>
2025-10-01 15:52:19 +0900

    Structure of .svelte file

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    // <script> block (optional)
+    // Only available in this component
+    // Apply TypeScript by adding the `lang="ts"` attribute
+</script>
+
+<!-- HTML tags with svelte syntax -->
+
+<style>
+    /* <style> block (optional) */
+    /* Only available in this component */
+</style>
