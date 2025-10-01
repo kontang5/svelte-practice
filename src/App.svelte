3439f0459536a3e66862c424dbc0b5648d721ba5
Jihoon <kontang5@icloud.com>
2025-10-01 20:07:11 +0900

    Basic template syntax

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
     // <script> block (optional)
     // Only available in this component
     // Apply TypeScript by adding the `lang="ts"` attribute
+import Basic from "./lib/Basic.svelte";
 </script>
 
 <!-- HTML tags with svelte syntax -->
+<Basic/>
 
 <style>
     /* <style> block (optional) */
---
+++ src/lib/Basic.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    let name: string = "Svelte";
+    let value: number = 123;
+    let imageURL: string = "https://github.com/sveltejs/branding/raw/master/svelte-logo.png";
+    let imageAlt: string = "Svelte Logo";
+    let next: number = 1;
+    let href: string = "https://svelte.dev"
+    let disabled: boolean = true;
+    let attributes: object = {
+        type: 'submit',
+        disabled: false,
+        class: 'button'
+    };
+</script>
+
+<!-- HTML tags -->
+
+<p class="text">Hello, Svelte!</p>
+<img src="https://github.com/sveltejs/branding/raw/master/svelte-logo.png" alt="Svelte Logo">
+
+<!-- Dynamic content -->
+
+<p>The current time is {new Date().toLocaleTimeString()}</p>
+<h1>Hello, {name}!</h1>
+<div>{(/^[A-Za-z]+$/).test(value.toString()) ? "OK" : "NG"}</div>
+
+<!-- Dynamic attributes -->
+
+<img src={imageURL} alt={imageAlt}>
+<a href="page/{next}">Next page</a>
+<!--<a href={href}>Svelte Official Webpage</a>-->
+<a {href}>Svelte Official Webpage</a>
+<button {disabled}>Out of stock</button>
+<!--<button type={attributes.type} disabled={disabled} class={attributes.class}>Button</button>-->
+<button {...attributes}>Button</button>
