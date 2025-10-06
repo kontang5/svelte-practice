c733e921fbc950d4a75fc19ffb00c86e1fdd1d3a
Jihoon <kontang5@icloud.com>
2025-10-07 07:20:52 +0900

    Component binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,9 @@
 <script>
-    let time;
+    import CustomSelect from "./lib/reactivity/CustomSelect.svelte";
+
+    let fruit = "apple";
 </script>
 
-<video
-        bind:currentTime={time}
-        controls
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-        width="320"
->
-    <track kind="captions">
-</video>
+<div>Choosing: {fruit}</div>
 
-<div>
-    {#each [1, 2, 3, 4, 5] as t}
-        <button on:click={() => (time = t)}>move to {t}</button>
-        {/each}
-</div>
-
-{#if time !== undefined}
-    <div>playing around {time}</div>
-{/if}
+<CustomSelect bind:selected={fruit} options={["apple", "banana", "orange"]} />
